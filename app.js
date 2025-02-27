ff60779942269e970233af725abd1b28149d7367
Finley Mwangola <finley.mwachia12@gmail.com>
2025-02-27 09:30:57 +0300

    install dotenv

---
+++ app.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send(`welcome to the subscription tracker api`);
+});
+app.listen(3000, () => {    
+  console.log("Subscription tracker apai is running on http://localhost:3000");
+} );
+
+export default app;
